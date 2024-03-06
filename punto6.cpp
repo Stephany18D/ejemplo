d6e9e182451a88db96d8e0320e0298da651bdb98
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-03-05 21:58:27 -0500

    puntos

--- punto6.cpp
+++ punto6.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-// Define la función factorial para calcular el factorial de un número
 double calcularFactorial(int n) {
     double resultado = 1;
     for(int i = 1; i <= n; i++) {
@@ -11,7 +10,6 @@ double calcularFactorial(int n) {
     return resultado;
 }
 
-// Función principal para ejecutar el ejercicio
 int ejercicio6() {
     int n;
     cout << "Introduce el numero de elementos usados en la aproximacion: ";
@@ -19,12 +17,10 @@ int ejercicio6() {
 
     double e = 0;
 
-    // Calcula 'e' aproximadamente utilizando la serie de Taylor
     for(int i = 0; i < n; i++) {
         e += 1 / calcularFactorial(i);
     }
 
-    // Muestra el resultado de 'e' aproximado
     cout << "e es aproximadamente: " << e << endl;
 
     return 0;
