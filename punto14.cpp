518d8e250c4c22bec4199c7043a2e752d7efd2b8
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-03-05 22:18:45 -0500

    puntos

---
+++ punto14.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// Función para verificar si un número es palíndromo
+bool esPalindromo(int num) {
+    int original = num;
+    int invertido = 0;
+
+    while(original > 0) {
+        invertido = invertido * 10 + original % 10;
+        original = original / 10;
+    }
+
+    return num == invertido;
+}
+
+int ejercicio14() {
+    int mayorPalindromo = 0;
+    int factor1 = 0, factor2 = 0;
+
+    for (int i = 100; i < 1000; i++) {
+        for (int j = 100; j < 1000; j++) {
+            int producto = i * j;
+            if (esPalindromo(producto) && producto > mayorPalindromo) {
+                mayorPalindromo = producto;
+                factor1 = i;
+                factor2 = j;
+            }
+        }
+    }
+
+    cout << factor1 << " * " << factor2 << " = " << mayorPalindromo << endl;
+
+    return 0;
+}
