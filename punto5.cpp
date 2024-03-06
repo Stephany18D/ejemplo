7a5964fd438344d5de5c58896290ecedce02fe9a
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-03-05 21:30:56 -0500

    puntos

--- punto5.cpp
+++ punto5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 int ejercicio5() {
     int tamano = 0;
@@ -6,36 +7,36 @@ int ejercicio5() {
     int espacios = 0;
     int asteriscos = 0;
 
-    std::cout << "Ingrese un numero impar para el tamaño del patrun: ";
-    std::cin >> tamano;
+    cout << "Ingrese un numero impar para el tamaño del patrun: ";
+    cin >> tamano;
 
     while (tamano % 2 == 0) {
-        std::cout << "Por favor ingrese un numero impar: ";
-        std::cin >> tamano;
+        cout << "Por favor ingrese un numero impar: ";
+        cin >> tamano;
     }
 
     for (i = 1; i <= tamano; i += 2) {
         espacios = (tamano - i) / 2;
         asteriscos = i;
         for (int j = 0; j < espacios; j++) {
-            std::cout << " ";
+            cout << " ";
         }
         for (int j = 0; j < asteriscos; j++) {
-            std::cout << "*";
+            cout << "*";
         }
-        std::cout << "\n";
+        cout << "\n";
     }
 
     for (i = tamano - 2; i > 0; i -= 2) {
         espacios = (tamano - i) / 2;
         asteriscos = i;
         for (int j = 0; j < espacios; j++) {
-            std::cout << " ";
+            cout << " ";
         }
         for (int j = 0; j < asteriscos; j++) {
-            std::cout << "*";
+            cout << "*";
         }
-        std::cout << "\n";
+        cout << "\n";
     }
 
     return 0;
